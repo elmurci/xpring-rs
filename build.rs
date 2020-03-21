9f86da1a8e6ca84ecfc8ef12a41225c43b99f9e4
Javi <elmurci@gmail.com>
2020-03-21 12:21:33 +0100

    Fix build.rs
    Ignore erros when cleaning the existing xpringjs file.
    It is normal that it might not be there.

--- build.rs
+++ build.rs
@@ -3,7 +3,14 @@ use std::{fs};
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
 
-    fs::remove_file(format!("{}/xpring.js", out_dir)).expect("Unable to clean file");
+    match fs::remove_file(format!("{}/xpring.js", out_dir)) {
+        Ok(result) => {
+           println!("Removing existing file... {:?}", result); 
+        },
+        Err(error) => {
+            println!("Error removing existing file... {:?}", error); 
+        }
+    }
 
     tonic_build::compile_protos("lib/ripple/xrp_ledger.proto").unwrap();
 

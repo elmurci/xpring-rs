75630088fb02cd7a17d1533b10c1fda0dcb849a7
Atul Bhosale <atul1bhosale@gmail.com>
2020-05-31 21:56:43 +0530

    Format code using 'cargo fmt'

--- src/ilpclient.rs
+++ src/ilpclient.rs
@@ -1,8 +1,8 @@
 use crate::i::prelude::*;
+use anyhow::{bail, Error};
 use fehler::throws;
 use tokio::runtime::{Builder, Runtime};
 use tonic::{metadata::MetadataValue, transport::Channel, Request};
-use anyhow::{bail, Error};
 
 #[derive(PartialEq, Debug)]
 pub enum IlpPaymentStatus {
@@ -33,7 +33,7 @@ pub struct IlpClient {
     rt: Runtime,
     ilp_client: IlpOverHttpServiceClient<tonic::transport::Channel>,
     balance_client: BalanceServiceClient<tonic::transport::Channel>,
-    account_id: &'static str
+    account_id: &'static str,
 }
 
 impl IlpClient {
@@ -44,25 +44,36 @@ impl IlpClient {
             .enable_all()
             .build()
             .unwrap();
-            if token.is_empty() {
-                bail!("token cannot be empty");
-            }
+        if token.is_empty() {
+            bail!("token cannot be empty");
+        }
         let channel = rt.block_on(Channel::from_static(url).connect())?;
         let bearer = format!("Bearer {}", token);
         //TODO is there a better way than creating two clients?
         let bal_token = MetadataValue::from_str(&bearer)?;
         let ilp_token = MetadataValue::from_str(&bearer)?;
-        let ilp_client =
-            IlpOverHttpServiceClient::with_interceptor(channel.clone(), move |mut ilp_req: Request<()>| {
-                ilp_req.metadata_mut().insert("authorization", ilp_token.clone());
+        let ilp_client = IlpOverHttpServiceClient::with_interceptor(
+            channel.clone(),
+            move |mut ilp_req: Request<()>| {
+                ilp_req
+                    .metadata_mut()
+                    .insert("authorization", ilp_token.clone());
                 Ok(ilp_req)
-            });
+            },
+        );
         let balance_client =
             BalanceServiceClient::with_interceptor(channel, move |mut balance_req: Request<()>| {
-                balance_req.metadata_mut().insert("authorization", bal_token.clone());
+                balance_req
+                    .metadata_mut()
+                    .insert("authorization", bal_token.clone());
                 Ok(balance_req)
             });
-        Self { rt, ilp_client, balance_client, account_id }
+        Self {
+            rt,
+            ilp_client,
+            balance_client,
+            account_id,
+        }
     }
 
     #[throws(_)]
@@ -82,12 +93,12 @@ impl IlpClient {
                     prepaid_amount: result.prepaid_amount,
                     clearing_balance: result.clearing_balance,
                 }
-            },
+            }
             Err(error) => {
                 // error returned is Unknown, not a lot of information...
                 bail!(format!("balance request failed: {:?}", error.code()));
             }
-        } 
+        }
     }
 
     #[throws(_)]
@@ -95,7 +106,7 @@ impl IlpClient {
         &mut self,
         destination_payment_pointer: String,
         amount: u64,
-        timeout_seconds: u64
+        timeout_seconds: u64,
     ) -> IlpSendResponse {
         let request = tonic::Request::new(SendPaymentRequest {
             destination_payment_pointer,
@@ -118,12 +129,12 @@ impl IlpClient {
                     amount_delivered: result.amount_delivered,
                     amount_sent: result.amount_sent,
                 }
-            },
+            }
             Err(_error) => {
                 // error returned is Unknown, not a lot of information...
                 bail!("payment send failed");
             }
-        } 
+        }
     }
 }
 
@@ -142,10 +153,7 @@ mod tests {
                 assert!(false);
             }
             Err(error) => {
-                assert_eq!(
-                    "token cannot be empty",
-                    error.to_string()
-                );
+                assert_eq!("token cannot be empty", error.to_string());
             }
         }
     }
@@ -206,11 +214,7 @@ mod tests {
     fn test_send_wrong_account() {
         let mut client = IlpClient::connect(DEFAULT_SERVER_URL, "test.foo.bar", "password")?;
         let original_amount = 12;
-        match client.send(
-            "$money/baz".to_owned(),
-            original_amount.clone(),
-            10,
-        ) {
+        match client.send("$money/baz".to_owned(), original_amount.clone(), 10) {
             Ok(_result) => {
                 assert!(false);
             }
